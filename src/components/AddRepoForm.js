31f74abfd326f55f631bbb572ee2cd802322d7bb
iTyphoons <hon.yang88@hotmail.com>
2020-06-22 16:53:11 +0530

    Add license in repo meta card (#735)

--- src/components/AddRepoForm.js
+++ src/components/AddRepoForm.js
@@ -1,18 +1,15 @@
-import React, {useRef, useState} from "react";
+import React, {useRef} from "react";
 import {InputButton} from "../styles/Button";
 import Input from "../styles/Input";
 import {CardPadding} from "../styles/Card";
 import {Flex} from "../styles/Grid";
 import api from "../lib/apiGraphQL";
 import {isValidRepoUrl} from "../lib/util";
-import {repoStatusCode} from "../lib/repoStatusCode";
-import {ErrorMessage} from "../styles/Typography";
 
 function AddRepoForm({goalsId, onGoalAdded}) {
   const urlRef = useRef(null);
-  const [error, setError] = useState(null);
 
-  const _handleGoalCreation = async(event) => {
+  const _handleGoalCreation = (event) => {
     event.preventDefault();
     if (!urlRef.current.value) {
       urlRef.current.focus();
@@ -21,23 +18,9 @@ function AddRepoForm({goalsId, onGoalAdded}) {
     }
 
     const [isValid, repoUrl] = isValidRepoUrl(urlRef.current.value.replace(/\s+/g, ""));
-    const statusCode = await repoStatusCode(repoUrl);
-
     if (!isValid) {
       urlRef.current.focus();
-      setError("Invalid GitHub repository!");
-      return;
-    }
-
-    if (statusCode === 404) {
-      urlRef.current.focus();
-      setError("Repository not found!");
-      return;
-    }
-
-    if (statusCode === 301) {
-      urlRef.current.focus();
-      setError("Repository has been moved to another location!");
+      console.warn("Invalid GitHub repository!");
       return;
     }
     api
@@ -46,7 +29,6 @@ function AddRepoForm({goalsId, onGoalAdded}) {
         onGoalAdded(response.data.gitHub.createIssue.issue);
         urlRef.current.value = "";
         urlRef.current.focus();
-        setError(null);
       })
       .catch(e => console.error(e));
   };
@@ -60,7 +42,6 @@ function AddRepoForm({goalsId, onGoalAdded}) {
             Add
           </InputButton>
         </Flex>
-        <ErrorMessage>{error}</ErrorMessage>
       </CardPadding>
     </form>
   );
--- src/lib/repoStatusCode.js
+++
@@ -1,9 +0,0 @@
-export async function repoStatusCode(repoUrl) {
-  const apiRepoUrl = `https://api.github.com/repos/${repoUrl}`;
-  const response = await fetch(apiRepoUrl);
-  if (response.redirected) {
-    return 301;
-  } else {
-    return response.status;
-  }
-}
